ea1c0ac44b46f9974b0fb1bc512f4dd7332fddcb
Armando <armandoxd_f.o.b@hotmail.com>
2022-11-29 11:27:47 -0700

    Initial Commit

---
+++ CoreDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataManager.swift
+//  SwiftUIExamen
+//
+//  Created by CCDM30 on 29/11/22.
+//
+
+import Foundation
+import CoreData
+
+init(){
+    
+}
---
+++ SwiftUIExamen/SwiftUIExamenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExamenApp.swift
+//  SwiftUIExamen
+//
+//  Created by CCDM30 on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExamenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
